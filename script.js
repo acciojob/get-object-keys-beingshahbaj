7481a266f0d8e7f6d9bb994df4951a7e251a4022
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-12-28 17:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const student = {
+  name: "John Doe"
+};
+
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+
+var keys = student.getKeys();
+console.log(keys); 
